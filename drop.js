402ac9050fed1af217e9a8a8c72b9d42a9f64cee
thelucklessmage <thelucklessmage@gmail.com>
2026-06-12 10:33:52 +0200

    Create drop.js

---
+++ drop.js
@@ -0,0 +1,10 @@
+const dropInfo = {
+    showDrop: true,
+    title_en: "Next drop",
+    title_it: "Prossimo drop",
+    date_en: "August 1st, 2026 · 18:00 Swiss time",
+    date_it: "1 agosto 2026 · 18:00 ora svizzera",
+    text_en: "New handmade resin dice will be available soon.",
+    text_it: "Nuovi dadi artigianali in resina saranno disponibili presto.",
+    link: "collection.html"
+};
